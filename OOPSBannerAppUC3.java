b2492c9e5e6bc87d07f86b4a2d27b27bdb7d9b94
Charvith <charvith03@icloud.com>
2026-03-09 23:02:21 +0530

    commit

---
+++ OOPSBannerAppUC3.java
@@ -0,0 +1,16 @@
+public class OOPSBannerAppUC3 {
+     public static void main(String[] var0) {
+
+System.out.println(String.join(" ","      ******      "   ,    "       ******      "  ,   "  ********      "   ,   "     ******      " ));
+System.out.println(String.join(" ","    **      **    "   ,    "     **      **    "  ,   "  **      **    "   ,   "   **            " ));
+System.out.println(String.join(" ","  **          **  "   ,    "   **          **  "  ,   "  **        **  "   ,   "  **             " ));
+System.out.println(String.join(" ","  **          **  "   ,    "   **          **  "  ,   "  **      **    "   ,   "    **           " ));
+System.out.println(String.join(" ","  **          **  "   ,    "   **          **  "  ,   "  ** ******     "   ,   "      ****       " ));
+System.out.println(String.join(" ","  **          **  "   ,    "   **          **  "  ,   "  **            "   ,   "           **    " ));
+System.out.println(String.join(" ","  **          **  "   ,    "   **          **  "  ,   "  **            "   ,   "            **   " ));
+System.out.println(String.join(" ","    **      **    "   ,    "     **      **    "  ,   "  **            "   ,   "          **     " ));
+System.out.println(String.join(" ","      ******      "   ,    "       ******      "  ,   "  **            "   ,   "    ******       " ));
+ 
+   }
+}
+
